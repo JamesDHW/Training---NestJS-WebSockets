d009276096727fa8ea133f450264be17a71d7b51
jamesdhw <jameshaworthwheatman@gmail.com>
2024-01-31 22:36:33 +0000

    feat: add handler for events being emitted from clients

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,9 +1,39 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class EventsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @WebSocketServer() server: Server;
+
+  // Hooks into the connection event
+  handleConnection(client: any, ...args: any[]) {
+    const { sockets } = this.server.sockets;
+
+    console.log(`Client id: ${client.id} connected`);
+    console.log(`Number of connected clients: ${sockets.size}`);
+  }
+
+  // Hooks into the disconnection event
+  handleDisconnect(client: any) {
+    console.log(`Cliend id:${client.id} disconnected`);
+  }
+
+  @SubscribeMessage('event')
+  handleEvent(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ): string {
+    console.log('Handling published event to Socket server');
+
+    // broadcast an event to all connections except the publishing socket
+    client.broadcast.emit('event');
+
+    return data;
   }
 }
